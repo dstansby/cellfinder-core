4ad41fe426a1b9f09167f697624b7f8984882b0d
David Stansby <dstansby@gmail.com>
2023-04-04 16:26:14 +0100

    Clean up dask test script

--- dask_tests/plane_filter.py
+++ dask_tests/plane_filter.py
@@ -1,38 +1,34 @@
-import dask.array as da
-from dask import delayed
-from dask.distributed import Client
-import numpy as np
+import webbrowser
 
-from cellfinder_core.detect.filters.plane.plane_filter import get_tile_mask
-from cellfinder_core.detect.filters.setup_filters import setup_tile_filtering
+from dask.distributed import Client
+
+from cellfinder_core.main import main
 from cellfinder_core.tools.IO import read_with_dask
 
 if __name__ == "__main__":
-    client = Client(threads_per_worker=1, n_workers=4)
-    print(client.dashboard_link)
+    client = Client()
+    webbrowser.open_new_tab(client.dashboard_link)
 
-    signal_data_path = "/Users/dstansby/brainglobe-data/test_brain_SK_AA_71_3_subset/ch01_subset"
+    signal_data_path = (
+        "/Users/dstansby/brainglobe-data/"
+        "test_brain_SK_AA_71_3_subset/ch01_subsubset"
+    )
 
     signal_array = read_with_dask(signal_data_path)
+    plane_size_mb = (
+        signal_array.itemsize
+        * signal_array.shape[1]
+        * signal_array.shape[2]
+        / 1000
+        / 1000
+    )
 
-    clipping_value, threshold_value = setup_tile_filtering(signal_array[0])
-    ball_z_size = 3
+    print(f"Plane size: {plane_size_mb:.02f} MB")
 
-    @delayed
-    def _get_tile_mask(plane):
-        return get_tile_mask(plane, clipping_value, threshold_value, 4, 0.2, 10)
-
-    @delayed
-    def _ball_filter(plane_masks: list):
-        pass
-
-    tile_masks = []
-    for plane in signal_array[:20]:
-        tile_masks.append(_get_tile_mask(plane))
-
-    ball_filtered_planes = []
-    for i in range(0, len(tile_masks) - ball_z_size):
-        ball_filtered_planes.append(_ball_filter(tile_masks[i:i+ball_z_size]))
-
-    ball_filtered_planes = delayed(ball_filtered_planes)
-    ball_filtered_planes.visualize(filename="ball_filtered.png")
+    main(
+        signal_array,
+        background_array=signal_array,
+        voxel_sizes=[5, 2, 2],
+        ball_z_size=15,
+        ball_xy_size=4,
+    )
